df2493b4c41e0e7adb1879f7b8ec4523970a0235
adamwang <adamw43081@gmail.com>
2021-07-13 16:54:05 -0700

    added main method.

--- analysisUsingPandas.py
+++ analysisUsingPandas.py
@@ -3,9 +3,10 @@ import matplotlib.pyplot as plt
 import pandas_datareader as pd
 import analysisUsingCSV
 
-
 # Uses the Pandas get_data_yahoo method to get the data of the specific asset. Returns the closing price and the day of the week.
 # dayOfWeek is a list of 0, 1, 2, 3, 4 that represent Monday, Tuesday, Wednesday, Thursday, and Friday respectively.
+
+
 def parseData(tickerName, start, end):
 
     closingPrice = []
@@ -34,38 +35,55 @@ def plotAverage(tickerName, start, end):
     return avg
 
 
-xaxis = np.arange(0, 5, 1)
-plot1 = plt.figure(1)
-s, = plt.plot(xaxis, plotAverage("SPY", "2020-7-12", "2021-7-12"), marker='.')
-g, = plt.plot(xaxis, plotAverage("GOOG", "2020-7-12", "2021-7-12"), marker='.')
-m, = plt.plot(xaxis, plotAverage("MSFT", "2020-7-12", "2021-7-12"), marker='.')
-a, = plt.plot(xaxis, plotAverage("AAPL", "2020-7-12", "2021-7-12"), marker='.')
-# t, = plt.plot(xaxis, plotAverage("TSLA", "2020-7-12", "2021-7-12"), marker='.')
-f, = plt.plot(xaxis, plotAverage("FB", "2020-7-12", "2021-7-12"), marker='.')
-n, = plt.plot(xaxis, plotAverage("NFLX", "2020-7-12", "2021-7-12"), marker='.')
-k, = plt.plot(xaxis, plotAverage("KO", "2020-7-12", "2021-7-12"), marker='.')
+def main():
+    xaxis = np.arange(0, 5, 1)
+    plot1 = plt.figure(1)
+    s, = plt.plot(xaxis, plotAverage(
+        "SPY", "2020-7-12", "2021-7-12"), marker='.')
+    g, = plt.plot(xaxis, plotAverage(
+        "GOOG", "2020-7-12", "2021-7-12"), marker='.')
+    m, = plt.plot(xaxis, plotAverage(
+        "MSFT", "2020-7-12", "2021-7-12"), marker='.')
+    a, = plt.plot(xaxis, plotAverage(
+        "AAPL", "2020-7-12", "2021-7-12"), marker='.')
+    # t, = plt.plot(xaxis, plotAverage("TSLA", "2020-7-12", "2021-7-12"), marker='.')
+    f, = plt.plot(xaxis, plotAverage(
+        "FB", "2020-7-12", "2021-7-12"), marker='.')
+    n, = plt.plot(xaxis, plotAverage(
+        "NFLX", "2020-7-12", "2021-7-12"), marker='.')
+    k, = plt.plot(xaxis, plotAverage(
+        "KO", "2020-7-12", "2021-7-12"), marker='.')
 
-plt.title(
-    "Average percent gain each weekday over the past two years")
-analysisUsingCSV.graphLabels(s, g, m, a, f, n, k)
+    plt.title(
+        "Average percent gain each weekday over the past two years")
+    analysisUsingCSV.graphLabels(s, g, m, a, f, n, k)
+
+    plot2 = plt.figure(2)
+    s, = plt.plot(xaxis, plotAverage(
+        "SPY", "2019-7-13", "2021-7-12"), marker='.')
+    g, = plt.plot(xaxis, plotAverage(
+        "GOOG", "2019-7-13", "2021-7-12"), marker='.')
+    m, = plt.plot(xaxis, plotAverage(
+        "MSFT", "2019-7-13", "2021-7-12"), marker='.')
+    a, = plt.plot(xaxis, plotAverage(
+        "AAPL", "2019-7-13", "2021-7-12"), marker='.')
+    # t, = plt.plot(xaxis, plotAverage("TSLA", "2019-7-13", "2021-7-12"), marker='.')
+    f, = plt.plot(xaxis, plotAverage(
+        "FB", "2019-7-13", "2021-7-12"), marker='.')
+    n, = plt.plot(xaxis, plotAverage(
+        "NFLX", "2019-7-13", "2021-7-12"), marker='.')
+    k, = plt.plot(xaxis, plotAverage(
+        "KO", "2019-7-13", "2021-7-12"), marker='.')
+
+    plt.title(
+        "Average percent gain each weekday over the past two years")
+    analysisUsingCSV.graphLabels(s, g, m, a, f, n, k)
+
+    plt.show()
 
 
-plot2 = plt.figure(2)
-s, = plt.plot(xaxis, plotAverage("SPY", "2019-7-13", "2021-7-12"), marker='.')
-g, = plt.plot(xaxis, plotAverage("GOOG", "2019-7-13", "2021-7-12"), marker='.')
-m, = plt.plot(xaxis, plotAverage("MSFT", "2019-7-13", "2021-7-12"), marker='.')
-a, = plt.plot(xaxis, plotAverage("AAPL", "2019-7-13", "2021-7-12"), marker='.')
-# t, = plt.plot(xaxis, plotAverage("TSLA", "2019-7-13", "2021-7-12"), marker='.')
-f, = plt.plot(xaxis, plotAverage("FB", "2019-7-13", "2021-7-12"), marker='.')
-n, = plt.plot(xaxis, plotAverage("NFLX", "2019-7-13", "2021-7-12"), marker='.')
-k, = plt.plot(xaxis, plotAverage("KO", "2019-7-13", "2021-7-12"), marker='.')
-
-plt.title(
-    "Average percent gain each weekday over the past two years")
-analysisUsingCSV.graphLabels(s, g, m, a, f, n, k)
-
-plt.show()
-
+if __name__ == '__main__':
+    main()
 
 # Debugging the difference between CSV data and Pandas data.
 # a = auc.parseData("GOOG2.csv")
